c0d646e6305540aba52ea0c67c02317ed5dc8470
kyi828 <ky2004828@gmail.com>
2025-04-24 13:23:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const apiKey = '41e44f5d31098d5ed0a2f1a5c6c9f482'; // This is a free-tier API key from OpenWeatherMap.org
+const my_location = 'Carmel, IN, Indiana';
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${my_location}&APPID=${apiKey}`;
+console.log(apiUrl);
+
+function init() {
+	$.getJSON(apiUrl, function(data) {
+	const temp = Math.round(data.main.temp - 273.15); // convert from Kelvin to Celsius
+	const description = data.weather[0].description;
+	
+	$('#temp').text(`${temp}°C`);
+	$('#description').text(description);
+	});
+
+	$('#weather-card').remove();
+	const weatherCardHtml = `
+	<div id="weather-card">
+		<h2 id="location">${my_location}</h2>
+		<p id="temp"></p>
+		<p id="description"></p>
+	</div>
+	`;
+	$('body').append(weatherCardHtml);
+}
+
+$(document).ready(init);
